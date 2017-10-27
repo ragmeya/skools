5abd2aea314df4cfb10d93e6e00648c038a5eb63
rrao <rrao@DTIPL-LT-079.davintaad.com>
2017-10-27 14:08:39 +0530

    test javadoc

--- src/main/java/com/skools/application/Run.java
+++ src/main/java/com/skools/application/Run.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * 
+ * @author rrao
+ *
+ */
 public class Run {
 
 	public static void main(String[] args) {
