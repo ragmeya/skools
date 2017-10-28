7b5798d14eac276379bf633f7b2eb893803c64e3
rrao <rrao@DTIPL-LT-079.davintaad.com>
2017-10-28 21:18:21 +0530

    spring boot with atlas mongodb

--- src/main/java/com/skools/rest/DashboardController.java
+++ src/main/java/com/skools/rest/DashboardController.java
@@ -1,14 +1,33 @@
 package com.skools.rest;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+import com.skools.dto.UserRequest;
+import com.skools.service.UserService;
+
+@Controller
+@RequestMapping("/dashboard")
 public class DashboardController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/hello")
+	public HttpEntity<String> hello() {
+		 return new ResponseEntity<>("Hello world", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+    public HttpEntity<String> createUser(@RequestBody UserRequest request) {
+        userService.createUser(request.getFirstName(), request.getLastName());
+        return new ResponseEntity<>("Created user", HttpStatus.OK);
     }
 
 }
